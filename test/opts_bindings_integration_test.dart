a98449ad69f3bd052240089035b5b90b44ee86b2
vik-borisov <vik-borisov@users.noreply.github.com>
2025-05-27 12:04:59 +0700

    Update bindings.yml and integration tests to use .dll files for Windows

--- test/opts_bindings_integration_test.dart
+++ test/opts_bindings_integration_test.dart
@@ -19,12 +19,12 @@ void main() {
     } else if (Platform.isLinux) {
       ffi.DynamicLibrary.open('linux/libssh2.so');
     } else if (Platform.isWindows) {
-      ffi.DynamicLibrary.open('windows/libssh2.lib');
+      ffi.DynamicLibrary.open('windows/libssh2.dll');
     }
 
     // Then load the main library
     if (Platform.isWindows) {
-      library = ffi.DynamicLibrary.open('windows/libgit2.lib');
+      library = ffi.DynamicLibrary.open('windows/libgit2.dll');
     } else if (Platform.isMacOS) {
       library = ffi.DynamicLibrary.open('macos/libgit2.dylib');
     } else {
@@ -48,25 +48,25 @@ void main() {
         );
         final initialSize = size.value;
 
-      // Set new size
-      final newSize = initialSize + 1024;
-      expect(
-        opts.git_libgit2_opts_set_mwindow_size(newSize),
-        equals(0),
-        reason: libgit2.getLastError()?.toString(),
-      );
+        // Set new size
+        final newSize = initialSize + 1024;
+        expect(
+          opts.git_libgit2_opts_set_mwindow_size(newSize),
+          equals(0),
+          reason: libgit2.getLastError()?.toString(),
+        );
 
-      // Verify size was changed
-      expect(
-        opts.git_libgit2_opts_get_mwindow_size(size),
-        equals(0),
-        reason: libgit2.getLastError()?.toString(),
-      );
-      expect(
-        size.value,
-        equals(newSize),
-        reason: libgit2.getLastError()?.toString(),
-      );
+        // Verify size was changed
+        expect(
+          opts.git_libgit2_opts_get_mwindow_size(size),
+          equals(0),
+          reason: libgit2.getLastError()?.toString(),
+        );
+        expect(
+          size.value,
+          equals(newSize),
+          reason: libgit2.getLastError()?.toString(),
+        );
 
         // Restore original size
         expect(
